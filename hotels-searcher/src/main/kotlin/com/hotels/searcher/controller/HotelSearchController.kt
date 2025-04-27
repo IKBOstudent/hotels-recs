e89891823c50c980860e1977cbc8018aca215830
IKBOstudent <maxpon225@gmail.com>
2025-04-27 17:26:54 +0300

    fix

--- hotels-searcher/src/main/kotlin/com/hotels/searcher/controller/HotelSearchController.kt
+++ hotels-searcher/src/main/kotlin/com/hotels/searcher/controller/HotelSearchController.kt
@@ -3,7 +3,7 @@ package com.hotels.searcher.controller
 import com.hotels.searcher.dto.HotelsSearchReq
 import com.hotels.searcher.service.HotelSearchService
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class HotelSearchController(
     private val searchService: HotelSearchService,
 ) {
-    @GetMapping("/search")
+    @PostMapping("/search")
     fun searchHotels(
         @Valid @RequestBody request: HotelsSearchReq,
     ) = searchService.searchHotels(request)
